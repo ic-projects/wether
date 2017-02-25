441193b05833d33396af3281eb84384854595466
Laurence <laurence.j.squires@gmail.com>
2017-02-25 15:26:37 +0000

    Updated contracts, added admin functions

--- Ethereum/contracts/Weather.sol
+++ Ethereum/contracts/Weather.sol
@@ -56,11 +56,16 @@ contract Weather {
 	function getPayoutQuote(int lat, int long, uint date, uint amount)
 	constant returns (uint payout) {
 
+		//TODO
+		return amount * 2;
 	}
 
 	function getWeatherResult(int lat, int long, uint date)
 	internal constant returns (bool didItRain) {
 
+
+		//TODO
+		return true;
 	}
 
 	function createInsurance(int lat, int long, uint date)
@@ -104,6 +109,38 @@ contract Weather {
 		}
 	}
 
+
+
+
+
+	//Owner functions
+
+	function forceCreateInsurance(int lat, int long, uint date) onlyOwner
+	payable returns (bool success) {
+		if(msg.value < minAmount) {
+			throw;
+		}
+
+		var payout = getPayoutQuote(lat, long, date, msg.value);
+		var i = Insurance(lat, long, payout, date, false, true);
+		accounts[msg.sender].insurances.push(i);
+	}
+
+	function setMinAmount(uint newMinAmount) onlyOwner {
+		minAmount = newMinAmount;
+	}
+
+	function destroy() onlyOwner {
+		suicide(owner);
+	}
+
+	modifier onlyOwner()
+    {
+        if (msg.sender != owner)
+            throw;
+
+        _;
+    }
 
 
 }
